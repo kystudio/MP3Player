cbcfaee0f7c54fc919e6a71d1b8c06e058670fa5
kystudio <yuan012308@hotmail.com>
2016-09-11 21:51:50 +0800

    first day

---
+++ app/src/main/java/com/kystudio/download/HttpDownloader.java
@@ -0,0 +1,104 @@
+package com.kystudio.download;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by 20236320 on 2016/8/14.
+ */
+public class HttpDownloader {
+    private URL url = null;
+
+    /**
+     * 根据URL下载文件，前提是文件内容是文本，函数的返回值是文件的内容。
+     *
+     * @param urlStr
+     * @return
+     */
+    public String downloadTxt(String urlStr) {
+        StringBuffer sb = new StringBuffer();
+        String line = null;
+        BufferedReader buffer = null;
+
+        try {
+            url = new URL(urlStr);
+
+            HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+            buffer = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+            while (null != (line = buffer.readLine())) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                buffer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 可以下载任何格式文件
+     * 返回整形（-1：下载文件出错；0：文件已存在；1：下载文件成功）
+     *
+     * @param urlStr
+     * @param path
+     * @param fileName
+     * @return
+     */
+    public int downloadFile(String urlStr, String path, String fileName) {
+        InputStream inputStream = null;
+
+        try {
+            FileUtils fileUtils = new FileUtils();
+
+            if (fileUtils.isFileExist(path + fileName)) {
+                return 0;
+            } else {
+                inputStream = getInputStreamFromUrl(urlStr);
+                File resultFile = fileUtils.write2SDFromInput(path, fileName, inputStream);
+                if (resultFile == null) {
+                    return -1;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        } finally {
+            try {
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return 1;
+    }
+
+    /**
+     * 根据URL得到输入流
+     *
+     * @param urlStr
+     * @return
+     * @throws MalformedURLException
+     * @throws IOException
+     */
+    public InputStream getInputStreamFromUrl(String urlStr) throws MalformedURLException, IOException {
+        url = new URL(urlStr);
+        HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = urlConn.getInputStream();
+
+        return inputStream;
+    }
+}
--- app/src/main/java/com/kystudio/mp3player/MP3ListActivity.java
+++ app/src/main/java/com/kystudio/mp3player/MP3ListActivity.java
@@ -1,28 +1,34 @@
 package com.kystudio.mp3player;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MP3ListActivity extends AppCompatActivity {
+import com.kystudio.download.HttpDownloader;
+
+public class MP3ListActivity extends ListActivity {
+    private static final int UPDATE = 1;
+    private static final int ABOUT = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp3_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_mp3_list, menu);
-        return true;
+
+        menu.add(0, UPDATE, 1, R.string.mp3list_update);
+        menu.add(0, ABOUT, 2, R.string.mp3list_about);
+        return super.onCreateOptionsMenu(menu);
+
+//        getMenuInflater().inflate(R.menu.menu_mp3_list, menu);
+//        return true;
     }
 
     @Override
@@ -33,10 +39,23 @@ public class MP3ListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == UPDATE) {
+            String xml = downloadXML("http://192.168.3.105:8080/MP3/resource.xml");
+
+            System.out.println(xml);
             return true;
+        } else if (id == ABOUT) {
+
+
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String downloadXML(String urlStr){
+        HttpDownloader httpDownloader = new HttpDownloader();
+        String result = httpDownloader.downloadTxt(urlStr);
+
+        return result;
     }
 }
